5368ba1b9b31d7c038c5877850d9b87216b8b22a
Anshuman212 <raianshuman201282@gmail.com>
2023-09-22 16:30:11 +0530

    updated now compare the output files with diff command

--- index.js
+++ index.js
@@ -5,31 +5,29 @@ const { default: test } = require('node:test');
 const { stringify } = require("querystring");
 const { type } = require("express/lib/response");
 const app = express();
-const port = 10045; //given in companion
-const path = "C:/Users/raian/Desktop/My_Shit/cp_codes";
+const port = 10045; 
+const path = "/home/darthrider/Desktop/cp_codes";
 
 app.use(express.urlencoded({extended:true}));
-app.use(express.json()); //parses json and puts it in req.body
+app.use(express.json()); 
 app.post('/',(req,res)=>{
-  // storing data from the req.body
   let data= req.body;
-  // console.log(data);
   let test = data.tests;
-  // console.log(test);
+  console.log(test);
   let input = "";
-  input += String(test.length) +"\n"; // taking the input of total number of test cases
-  // console.log(input);
+  let output="";
+  input += String(test.length) +"\n";
   test.forEach((tests) => {
-    // adding all the inputs to the input name string
     input += tests.input;
+    output+=tests.output;
   });
-
-// now writing the inputs to the file
+  fs.writeFile(path +'/output.txt', output,"utf-8", (e)=>{
+    if(e) console.log(e);
+  })
 fs.writeFile(path +'/input.txt', input,"utf-8", (e)=>{
   if(e) console.log(e);
 })
-
-console.log('Parsing Success');
+console.log('Test Case Parsed');
 });
 
 
